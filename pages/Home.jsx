30221aaf22f97fc9bfe19650eec75838869b120b
Syed Yaseen <101188840+syedyaseen12@users.noreply.github.com>
2022-04-05 11:49:50 +0530

    Add files via upload

---
+++ pages/Home.jsx
@@ -0,0 +1,13 @@
+import UserResult from "../Components/Users/UserResult"
+
+function Home () {
+    return(
+      
+        <>
+         {/* SEARCH COMPONENT */}
+      <UserResult/>
+        </>
+    )
+}
+export default Home
+                             
